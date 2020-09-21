e62b1427904e0ad2106cbeb47ee3ab1ce800a65a
darkknight77 <shaiklatheefmrf98@gmail.com>
2020-09-21 21:15:02 +0530

    Spring boot version 2.3.4.RELEASE

--- DoctorsSprBoot/src/main/java/org/corona/DoctorController.java
+++ DoctorsSprBoot/src/main/java/org/corona/DoctorController.java
@@ -34,33 +34,38 @@ public class DoctorController {
 		return new ModelAndView("doctors-list", "doctorsList", doctorsList);
 	}
 
-	
-	  @RequestMapping(value = "searchDoctor", method = RequestMethod.GET) public
-	  ModelAndView searchDoctors(@RequestParam("doctorName") String doctorName) {
-	  logger.info("Getting the all Doctors from search."); List<DoctorModel>
-	  doctorsList = doctorService.searchDoctors(doctorName);
-	  logger.info(doctorsList); return new ModelAndView("doctors-list",
-	  "doctorsList", doctorsList); }
-	  
-	  @RequestMapping(value = "editDoctor", method = RequestMethod.GET) public
-	  ModelAndView editDoctor(@RequestParam("id") int id,@ModelAttribute
-	  DoctorModel doctor) { doctor = doctorService.getDoctor(id); return new
-	  ModelAndView("doctor-form","doctorObject",doctor); }
-	  
-	  @RequestMapping(value = "updateDoctor", method = RequestMethod.POST) public
-	  ModelAndView updateDoctor(@ModelAttribute DoctorModel doctor) {
-	  logger.info("Saving the Doctor Data : "+doctor);
-	  doctorService.updateDoctor(doctor); return new ModelAndView("redirect:/"); }
-	  
-	  @RequestMapping(value = "createDoctor", method = RequestMethod.GET) public
-	  ModelAndView createDoctor() { logger.info("creating the Doctor Data : ");
-	  return new ModelAndView("doctor-form","doctorObject",new DoctorModel()); }
-	  
-	  @RequestMapping(value = "deleteDoctor", method = RequestMethod.GET) public
-	  ModelAndView deleteDoctor(@RequestParam int id) {
-	  logger.info("deleting the Doctor Data : ");  doctorService.deleteDoctor(id); return new
-	  ModelAndView("redirect:/"); }
-	  
-	  
-	 
+	@RequestMapping(value = "searchDoctor", method = RequestMethod.GET)
+	public ModelAndView searchDoctors(@RequestParam("doctorName") String doctorName) {
+		logger.info("Getting the all Doctors from search.");
+		List<DoctorModel> doctorsList = doctorService.searchDoctors(doctorName);
+		logger.info(doctorsList);
+		return new ModelAndView("doctors-list", "doctorsList", doctorsList);
 	}
+
+	@RequestMapping(value = "editDoctor", method = RequestMethod.GET)
+	public ModelAndView editDoctor(@RequestParam("id") int id, @ModelAttribute DoctorModel doctor) {
+		doctor = doctorService.getDoctor(id);
+		return new ModelAndView("doctor-form", "doctorObject", doctor);
+	}
+
+	@RequestMapping(value = "updateDoctor", method = RequestMethod.POST)
+	public ModelAndView updateDoctor(@ModelAttribute DoctorModel doctor) {
+		logger.info("Saving the Doctor Data : " + doctor);
+		doctorService.updateDoctor(doctor);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping(value = "createDoctor", method = RequestMethod.GET)
+	public ModelAndView createDoctor() {
+		logger.info("creating the Doctor Data : ");
+		return new ModelAndView("doctor-form", "doctorObject", new DoctorModel());
+	}
+
+	@RequestMapping(value = "deleteDoctor", method = RequestMethod.GET)
+	public ModelAndView deleteDoctor(@RequestParam int id) {
+		logger.info("deleting the Doctor Data : ");
+		doctorService.deleteDoctor(id);
+		return new ModelAndView("redirect:/");
+	}
+
+}
